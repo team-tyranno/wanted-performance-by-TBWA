5123424942e65bc066b76f9bbfa96a3f1c2751bb
jambottle <jaewon.walks@gmail.com>
2022-02-23 07:00:03 +0900

    feat: Footer 컴포넌트 생성, 마크업 및 등록

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,11 @@ import GNB from './components/GNB/index.js';
 import Video from './components/Video/index.js';
 import Carousel from './components/Carousel/index.js';
 import Banner from './components/Banner/index.js';
+import Footer from './components/Footer/index.js';
 import { $ } from './utils/index.js';
 
 new GNB($('#navbar'));
 new Video($('#video'));
 new Carousel($('#carousel'));
 new Banner($('#banner'));
+new Footer($('#footer'));
